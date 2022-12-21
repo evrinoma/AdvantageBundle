3d9f90d3649ebcec256dc2b3078c67a75d8b40c0
Nikolay Nikolaev <evrinoma@gmail.com>
2022-12-21 11:44:15 +0300

    fix factory

--- src/Factory/Advantage/FactoryInterface.php
+++ src/Factory/Advantage/FactoryInterface.php
@@ -11,7 +11,7 @@ declare(strict_types=1);
  * file that was distributed with this source code.
  */
 
-namespace Evrinoma\AdvantageBundle\Factory;
+namespace Evrinoma\AdvantageBundle\Factory\Advantage;
 
 use Evrinoma\AdvantageBundle\Dto\AdvantageApiDtoInterface;
 use Evrinoma\AdvantageBundle\Model\Advantage\AdvantageInterface;
